6afc0c9f3afa54a5f4c8171d732297e3da81c5fd
John Paul Lopez <jpblopez05@gmail.com>
2023-09-15 15:54:42 +0800

    support for multiple users

--- dataPull.js
+++ dataPull.js
@@ -3,10 +3,21 @@ const cron = require('node-cron');
 const { cxnInit } = require('./cxn.service');
 const { skInit } = require('./sk.service');
 
+const cxnUsers = [
+  {
+    email: 'rubi@aka-extensions.com',
+    password: 'sdfLKNl34$jhndJ',
+  },
+  {
+    email: 'sean@aka-extensions.com',
+    password: 'fsdfJKHjknbkl235$',
+  },
+];
 const fetchData = async () => {
   try {
     await skInit();
-    await cxnInit();
+    await cxnInit({ users: cxnUsers });
+    console.log('--DONE--');
   } catch (error) {
     console.error('Error:', error.message);
   }
