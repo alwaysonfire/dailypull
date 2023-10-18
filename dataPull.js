803c237a46ef6aabf60ca6be12773477df84513b
John Paul Lopez <jpblopez05@gmail.com>
2023-10-18 17:07:25 +0800

    oct 5 for cxn

--- dataPull.js
+++ dataPull.js
@@ -22,15 +22,15 @@ const cxnUsers = [
 const fetchData = async () => {
   try {
     const fetchStart = new Date();
-    const skResult = await skInit();
-    // const cxnResult = await cxnInit({ users: cxnUsers });
+    // const skResult = await skInit();
+    const cxnResult = await cxnInit({ users: cxnUsers });
     const fetchEnd = new Date();
     const timeToProcess = (fetchEnd - fetchStart) / 1000;
 
     const toWrite =
       JSON.stringify({
-        skResult,
-        // cxnResult,
+        // skResult,
+        cxnResult,
         fetchStart,
         fetchEnd,
         timeToProcess,
